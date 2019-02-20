6f8833d807c229b39160e1be891630bf5443415d
rafaelBadan <rafael.badan@gmail.com>
2019-02-20 11:33:32 -0300

    adds connect-flash and config main alerts

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -11,12 +11,12 @@ class SessionController {
     const user = await User.findOne({ where: { email } })
 
     if (!user) {
-      console.log('Usuário não encontrado')
+      req.flash('error', 'Usuário não encontrado')
       return res.redirect('/')
     }
 
     if (!(await user.checkPassword(password))) {
-      console.log('senha incorreta')
+      req.flash('error', 'senha incorreta')
       return res.redirect('/')
     }
 
