cdaea3bf43e6fc302c3f0eec6d4cbc015cd7829d
rafaelBadan <rafael.badan@gmail.com>
2019-02-20 09:50:17 -0300

    creates signin view and SessionController

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,13 @@ const upload = require('multer')(multerConfig)
 const routes = express.Router()
 
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
+
+routes.get('/', SessionController.create)
+routes.post('/signin', SessionController.signin)
 
 routes.get('/signup', UserController.createNew)
 routes.post('/signup', upload.single('avatar'), UserController.save)
 
+routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
 module.exports = routes
